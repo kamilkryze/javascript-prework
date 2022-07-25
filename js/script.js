c2b3420fd2bb6c11d793aedb3d19f747c3d604f5
Kamil Kryze <kryzekamil@gmail.com>
2022-07-25 20:55:28 +0200

    paper fixed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 var computerMove, playerMove;
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-computerMove = 'papier';
-printMessage('Zagrałem' + playerMove '! Jeśli Twój ruch to kamień, to przegrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałem' + playerMove);
 
 var item, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-printMessage('Wylosowana liczba to: ' + randomNumber);
-randomNumber = Math.floor(Math.random() * 9 + 11);
 printMessage('Wylosowana liczba to: ' + randomNumber);
