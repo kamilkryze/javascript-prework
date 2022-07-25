0dd5e82b219b4626c02e86d5c579d85a61bb3301
Kamil Kryze <kryzekamil@gmail.com>
2022-07-25 21:07:39 +0200

    losowanie i logika

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-var computerMove, playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Zagrałem' + playerMove);
-
-var item, randomNumber;
+var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-printMessage('Wylosowana liczba to: ' + randomNumber);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '2') {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
