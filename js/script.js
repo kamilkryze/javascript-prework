247e0a55c8f2bee497bdd068bb5c6e64b04930bc
Kamil Kryze <kryzekamil@gmail.com>
2022-07-25 20:17:18 +0200

    exercise paper

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+computerMove = 'papier';
+printMessage('Zagrałem' + playerMove '! Jeśli Twój ruch to kamień, to przegrywasz!');
