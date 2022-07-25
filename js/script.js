caf013df74c80416df7a7bbf160edbb1ea80f15b
Kamil Kryze <kryzekamil@gmail.com>
2022-07-25 20:36:39 +0200

    randomNumber

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 computerMove = 'papier';
 printMessage('Zagrałem' + playerMove '! Jeśli Twój ruch to kamień, to przegrywasz!');
+
+var item, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+randomNumber = Math.floor(Math.random() * 9 + 11);
+printMessage('Wylosowana liczba to: ' + randomNumber);
