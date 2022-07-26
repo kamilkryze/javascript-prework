4fcc186c38abf101ab794f4a8262319a57ce2a7f
Kamil Kryze <kryzekamil@gmail.com>
2022-07-26 22:29:22 +0200

    dodanie guzików

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber;
+var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber, buttonRock, buttonPaper, buttonScissors;
 
-/**
+buttonRock = document.getElementById('button-Rock');
+buttonPaper = document.getElementById('button-paper');
+buttonScissors = document.getElementById('button-scissors');
+
+function buttonClicked(argButtonName){
+    clearMessages();
+    console.log(argButtonName + ' został kliknięty.');
+}
+
+    /**
  * Describe this function...
  */
 function getMoveName(argMoveId) {
@@ -35,12 +44,23 @@ function displayResult(argPlayerMove, argComputerMove) {
   }
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+playerMove = argButtonName;
 console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = getMoveName(playerInput);
 console.log('ruch gracza to: ' + playerMove);
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
+
+
+buttonRock.addEventListener('click', function(){
+    buttonClicked('Rock button')
+});
+buttonPaper.addEventListener('click', function(){
+    buttonClicked('Paper button')
+});
+buttonScissors.addEventListener('click', function(){
+    buttonClicked('Scissors button')
+});
